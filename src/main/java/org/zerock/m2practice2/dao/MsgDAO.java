c1cf208e13928b168d312d527a8bd756ebfaddd2
songyikim0911 <ksi6470@gmail.com>
2021-08-15 20:09:04 +0900

    a

--- src/main/java/org/zerock/m2practice2/dao/MsgDAO.java
+++ src/main/java/org/zerock/m2practice2/dao/MsgDAO.java
@@ -1,4 +1,34 @@
 package org.zerock.m2practice2.dao;
 
-public class MsgDAO {
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+import org.zerock.m2practice2.dto.MsgDTO;
+
+@Log4j2
+public enum MsgDAO {
+
+
+    INSTANCE;
+
+
+    public static final String SQL_INSERT = "insert into tbl_msg (who,whom,content) values (?,?,?)";
+
+
+    public void insert(MsgDTO msgDTO) throws RuntimeException{
+        new JdbcTemplate(){
+            @Override
+            protected void execute() throws Exception {
+                int idx = 1;
+                preparedStatement = connection.prepareStatement(SQL_INSERT);
+                preparedStatement.setString(idx++, msgDTO.getWho());
+                preparedStatement.setString(idx++, msgDTO.getWhom());
+                preparedStatement.setString(idx++, msgDTO.getContent());
+
+                int count = preparedStatement.executeUpdate();
+                log.info("count:" + count);
+            }
+        }.makeAll();
+    }
+
+
 }
--- src/main/java/org/zerock/m2practice2/dto/MsgDTO.java
+++ src/main/java/org/zerock/m2practice2/dto/MsgDTO.java
@@ -1,5 +1,20 @@
 package org.zerock.m2practice2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class MsgDTO {
 
+    private Long mno;
+    private String who, whom, content;
+    private Timestamp regdate, opendate;
 }
